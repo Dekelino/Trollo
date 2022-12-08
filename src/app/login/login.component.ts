96e824752662a24cac2f863667d2bcb3e9f040b1
TFs112 <benjamin.orolin@kosickaakademia.sk>
2022-12-08 12:32:35 +0100

    Frontend + Backend Working

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Auth } from 'src/entities/auth';
-import { Router } from '@angular/router'
+
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,12 @@ export class LoginComponent {
   hide = true
   auth = new Auth()
   
-  constructor(){
+  constructor(private usersService : UsersService){
 
   }
 
 
   onSubmit(){   
+    this.usersService.login(this.auth).subscribe()
   }
 }
