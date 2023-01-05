8da670a6b2ade87b408ce22de5b8b9fdb9b8e189
Dekľo <106304843+Dekelino@users.noreply.github.com>
2023-01-05 22:53:25 +0100

    fixed but not gooood

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -3,13 +3,17 @@ describe('template spec', () => {
   it('Should login and register user', () => {
     cy.visit('./login')
 
-    cy.get('input[name="username"]').type('TestUsername')
-    cy.get('input[name="password"').type('heslojeden23')
-    cy.get('button').contains("Login").click({force:true})
-    cy.wait(2000)
-    cy.get('button').contains("Logout").click()
-
-
+    cy.get('input[name="username"]')
+    .type('TestUseName')
+    cy.get('input[name="password"')
+    .type('heslojeden23')
+    cy.get('button').contains("Login")
+    .click({force:true})
+    cy.visit('./homePage')
+    cy.wait(10000)
+    cy.get('button').contains("Logout")
+    .should('be.visible')
+    .click()
 
   })
 })
