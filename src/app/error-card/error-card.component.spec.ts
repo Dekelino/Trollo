1c322c2086bc041f9165bf8e2ea78f78477856bb
Dekľo <106304843+Dekelino@users.noreply.github.com>
2023-02-08 11:58:39 +0100

    added edit window

--- src/app/error-card/error-card.component.spec.ts
+++ src/app/error-card/error-card.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TaskComponent } from './task.component';
+import { ErrorCardComponent } from './error-card.component';
 
-describe('TaskComponent', () => {
-  let component: TaskComponent;
-  let fixture: ComponentFixture<TaskComponent>;
+describe('ErrorCardComponent', () => {
+  let component: ErrorCardComponent;
+  let fixture: ComponentFixture<ErrorCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskComponent ]
+      declarations: [ ErrorCardComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TaskComponent);
+    fixture = TestBed.createComponent(ErrorCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
